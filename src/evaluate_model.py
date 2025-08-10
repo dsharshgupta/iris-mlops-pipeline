bc99961d2cd22d8d576182a617463405644d7194
dsharshgupta <109061719+dsharshgupta@users.noreply.github.com>
2025-08-10 13:39:22 +0530

    Update evaluate_model.py

--- src/evaluate_model.py
+++ src/evaluate_model.py
@@ -67,7 +67,7 @@ def evaluate_model(model_path='models/iris_model.pkl', scaler_path='models/scale
             explainer = shap.KernelExplainer(model.predict_proba, X_summary)
             shap_values = explainer.shap_values(X_scaled)
 
-            shap.summary_plot(shap_values[2], X, show=False)
+            shap.summary_plot(shap_values[2], X_scaled, show=False)
             plt.title('SHAP Summary Plot for Virginica')
             shap_plot_path = 'models/shap_summary_plot_virginica.png'
             plt.savefig(shap_plot_path)
